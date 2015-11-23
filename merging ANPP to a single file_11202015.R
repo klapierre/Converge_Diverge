a52acb76134a0e59fbd0a9fcbce159c86d1e42a0
klapierre <lapierre.kimberly@gmail.com>
2015-11-23 15:44:14 -0800

    Add code to get treatment column in ANPP file and to find mean ANPP of control plots.

--- merging ANPP to a single file_11202015.R
+++ merging ANPP to a single file_11202015.R
@@ -5,102 +5,102 @@ library(reshape2)
 library(tidyr)
 library(dplyr)
 
-nme<-read.delim("Alps_NME_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+# nme<-read.delim("Alps_NME_anpp.txt")%>%
+#   select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 watering<-read.delim("ANG_watering_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 clonal<-read.delim("ASGA_Clonal_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 exp1<-read.delim("ASGA_Exp1_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 lind<-read.delim("BAY_LIND_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 events<-read.delim("Bt_EVENT2_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 rmapc<-read.delim("CAU_RMAPC_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 biocon<-read.delim("CDR_biocon_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 e001<-read.delim("CDR_e001_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 e002<-read.delim("CDR_e002_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 megarich<-read.delim("CEH_Megarich_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 imgers<-read.delim("IMGERS_Yu_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 imagine<-read.delim("CLE_imagine_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 nsfc<-read.delim("DL_NSFC_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 wapaclip<-read.delim("KAEFS_WaPaClip_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 t7<-read.delim("KBS_T7_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
-kgfert<-read.delim("KLUG_KGFert_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+kgfert<-read.delim("KLU_KGFert_anpp.txt")%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 bgp<-read.delim("KNZ_BGP_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 irg<-read.delim("KNZ_IRG_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 pplots<-read.delim("KNZ_PPLOTS_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 ramps<-read.delim("KNZ_Ramps_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 rhps<-read.delim("KNZ_RHPs_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 nfert<-read.delim("NWT_246NFert_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 bowman<-read.delim("NWT_bowman_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 oface<-read.delim("ORNL_FACE_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 tide<-read.delim("PIE_Tide_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 snfert<-read.delim("SEV_NFert_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 wenndex<-read.delim("SEV_WENNDEx_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 uk<-read.delim("SKY_UK_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 fireplots<-read.delim("MAERC_fireplots_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 wet<-read.delim("NANT_wet_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 gb<-read.delim("NGBER_gb_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 rio<-read.delim("RIO_interaction_anpp.txt")%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, plot_id, anpp)
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, anpp)
 mnr<-read.delim("MNR_watfer_anpp.txt")%>%
-  select(site_code, project_name, treatment_year, calendar_year, plot_id, anpp)%>%
+  select(site_code, project_name, treatment_year, calendar_year, treatment, plot_id, anpp)%>%
   mutate(community_type=0)
 
-anpp <- rbind(bgp, biocon, bowman, clonal, e001, e002, events, exp1, fireplots, gb, imagine, imgers, irg, kgfert, lind, megarich, mnr, nfert, nme, nsfc, oface, pplots, ramps, rhps, rio, rmapc, snfert, t7, tide, uk, wapaclip, watering, wenndex, wet)
+anpp <- rbind(bgp, biocon, bowman, clonal, e001, e002, events, exp1, fireplots, gb, imagine, imgers, irg, kgfert, lind, megarich, mnr, nfert, nsfc, oface, pplots, ramps, rhps, rio, rmapc, snfert, t7, tide, uk, wapaclip, watering, wenndex, wet)
 
 write.csv(anpp, 'C:\\Users\\Kim\\Dropbox\\working groups\\converge diverge working group\\converge_diverge\\datasets\\LongForm\\ANPP_11202015.csv')
