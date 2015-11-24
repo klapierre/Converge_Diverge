6438ea607e49847c09db624b5d6f917b6bfd5f8c
mavolio <meghan.avolio@gmail.com>
2015-11-24 17:08:57 -0500

    getting rarefy code to work

--- site_information_11202015.R
+++ site_information_11202015.R
@@ -13,7 +13,7 @@ ExpInfo <- read.csv("SpeciesRelativeAbundance_Nov2015.csv")%>%
 ExpList<-ExpInfo%>%
   select(site_code, project_name)%>%
   unique()
-write.csv(ExpList, "Experiment_List.csv")
+#write.csv(ExpList, "Experiment_List.csv")
 
 #Getting control ANPP
 # ANPP<-read.csv("ANPP_Nov2015.csv")
@@ -54,26 +54,26 @@ ExpLength<-ExpInfo%>%
   group_by(site_code, project_name, community_type)%>%
   summarize(experiment_length=max(treatment_year))
 
-siteinfo2<-merge(siteinfo, experiment_length, by=c("site_code","project_name","community_type"), all=T)
-
-species_num<-species%>%
-  tbl_df()%>%
-  group_by(site_code, project_name, community_type, genus_species)%>%
-  summarize(rich=length(abundance))%>%
-  tbl_df()%>%
-  group_by(site_code, project_name, community_type)%>%
-  summarize(rich=length(rich))
-
-site_info3<-merge(siteinfo2, species_num, by=c("site_code","project_name","community_type"), all=T)
-
 ##need to rarefy species. not sure how to do this with cover data.
 
 
 ##calculate chao richness and rarefied richness for each site
 #import species data
-species <- read.csv("SpeciesRawAbundance_11202015.csv")%>%
+species <- read.csv("SpeciesRawAbundance_Nov2015.csv")%>%
   select(site_code, project_name, community_type, plot_id, calendar_year, genus_species, abundance)%>%
-  mutate(exp=paste(site_code, project_name, community_type, sep='::'))
+  mutate(exp=paste(site_code, project_name, community_type, sep='::'))%>%
+  #get rid of duplicate species within a plot and year in the dataset; once we contact the dataowners, this step will no longer be needed
+  tbl_df()%>%
+  group_by(exp,site_code, project_name, community_type, calendar_year, plot_id, genus_species)%>%
+  summarise(abundance=mean(abundance))
+
+SampleIntensity<-species%>%
+  tbl_df()%>%
+  group_by(exp, plot_id, calendar_year)%>%
+  summarize(SampleIntensity=length(abundance))%>%
+  tbl_df()%>%
+  group_by(exp)%>%
+  summarize(SampleIntensity=length(SampleIntensity))
 
 exp=species%>%
   select(exp)%>%
@@ -85,7 +85,8 @@ estimatedRichness=data.frame(row.names=1)
 for(i in 1:length(exp$exp)) {
 
   #creates a dataset for each unique experiment
-  subset <- species[species$exp==as.character(site$exp[1]),]%>%
+  subset <- species%>%
+    filter(exp==exp$exp[i])%>%
     select(exp, plot_id, calendar_year, genus_species, abundance)
   
   #transpose data into wide form
@@ -93,39 +94,34 @@ for(i in 1:length(exp$exp)) {
     spread(genus_species, abundance, fill=0)
   
   #calculate species accumulation curves
-  pool <- poolaccum(speciesData[4:ncol(speciesData)], permutations=100)
-  chao <- summary(pool, display='chao') #this gives us estimated richness from 1-X samples, but then how do we get that into a dataframe?
-  chao <- as.data.frame(chao) #this doesn't remotely work; I also tried making a matrix first
+  pool <- poolaccum(speciesData[,4:ncol(speciesData)], permutations=100)
+  chao <- as.data.frame(as.matrix(pool$chao))#this gives us estimated richness from 1-X samples
+  chao$aveChao<-rowMeans(chao)
+  chao$n<-row.names(chao)
+  chao$exp<-exp$exp[i]
+  chao2<-chao%>%
+    select(exp,n, aveChao)
   
+  #rbind back
+  estimatedRichness<-rbind(chao2, estimatedRichness)
+}
+
   #calculate rarefied value
   #we should get all of the chao richness estimates for samples 1-X for each experiment, cbind them all together, find the min sample number across all experiments, and then use that to get rarefied richness for each of the experiments
   
   #in theory we could also get estimated richness from our pool/species accumulation curves by extrapolating out beyond the number of samples collected
 
-}
+pplots<-species%>%
+  filter(project_name=="pplots")%>%
+  spread(genus_species, abundance, fill=0)
 
-
-
-
-#sample from vegan documentation - not remotely helpful
-data(dune)
-data(dune.env)
-attach(dune.env)
-pool <- specpool(dune, Management)
-pool
-op <- par(mfrow=c(1,2))
-boxplot(specnumber(dune) ~ Management, col="hotpink", border="cyan3",
-        notch=TRUE)
-boxplot(specnumber(dune)/specpool2vect(pool) ~ Management, col="hotpink",
-        border="cyan3", notch=TRUE)
-par(op)
-data(BCI)
-## Accumulation model
-pool <- poolaccum(BCI)
-summary(pool, display = "chao")
-plot(pool)
-## Quantitative model
-estimateR(BCI[1:5,])
+specpool(pplots[,7:ncol(pplots)])
+pool<-poolaccum(pplots[,7:ncol(pplots)])
+chao <- as.data.frame(as.matrix(pool$chao))
+chao$average<-rowMeans(chao)
+chao$n<-row.names(chao)
+chao2<-chao%>%
+  select(n, average)
 
 
 
