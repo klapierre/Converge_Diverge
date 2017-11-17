39d4c933204e141edcc8417327b4ad9683ef07ef
Meghan <meghan.avolio@gmail.com>
2017-11-17 10:49:26 -0500

    looking at data quality for each corre dataset site

--- Precip_data.R
+++ Precip_data.R
@@ -4,19 +4,27 @@ library(tidyverse)
 library(maps)
 library(mapdata)
 library(mapproj)
+library(geosphere)
+library(lubridate)
 
 #get list of global historical climate network daily stations
 #stations<-ghcnd_stations()
-write.csv(stations, "~/Dropbox/converge_diverge/datasets/LongForm/climate/ghcn_stations.csv")
+#write.csv(stations, "~/Dropbox/converge_diverge/datasets/LongForm/climate/ghcn_stations.csv")
 
 #read in data
 stations<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/climate/ghcn_stations.csv")
+stations<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\climate\\ghcn_stations.csv")
 
 stations_subset<-stations%>%
-  select(id, latitude, longitude, name)%>%
-  unique()
+  select(id, latitude, longitude, name, element, first_year, state, last_year)%>%
+  filter(element=="PRCP")%>%
+  unique()%>%
+  filter(first_year<1983&last_year>2015)
 
-correloc<-read.csv("~/Dropbox/converge_diverge/datasets/LongForm/climate/siteList_LatLong.csv")
+station_list<-stations_subset%>%
+  select(id, latitude, longitude, name)
+
+correloc<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\climate\\siteList_LatLong.csv")
 
 ##ploting stations
 map <- borders("world", colour="gray80", fill="gray80") # create a layer of borders
@@ -24,13 +32,79 @@ map <- borders("world", colour="gray80", fill="gray80") # create a layer of bord
 ggplot() +
   map+
   geom_point(data=stations_subset, aes(x=longitude, y=latitude))+
-  geom_point(data=correloc, aes(x=Longitue,y=Lattitue),col="red")
+  geom_point(data=correloc, aes(x=longitude,y=latitude),col="red")
 
 #pairwise ditances
-long<-nb2[,32]
-lat<-nb2[,31]
-longlat<-cbind(long,lat)
-nb_dist1<-distm(longlat,fun=distCosine)/1000#distance btwn nb in KM
-#library(geosphere)
+tocompare<-rbind(station_list, correloc)
 
-gDistance(sp.mydata, byid=T)#library rgeos
+long<-tocompare[,3]
+lat<-tocompare[,2]
+longlat<-cbind(long,lat)
+dist1<-as.data.frame(distm(longlat,fun=distCosine))/1000
+
+colnames(dist1)<-tocompare$id
+dist2<-cbind(tocompare, dist1)
+
+dist3<-dist2[c(13968:14018),]
+
+dist4<-dist3%>%
+  gather(station, distance, 5:14022)
+
+closest_stations<-dist4%>%
+  filter(distance<200&distance!=0)%>%
+  group_by(name)%>%
+  mutate(rank=rank(distance))%>%
+  filter(rank<4)%>%
+  filter(station!="corre5"&station!='corre6'&station!="corre12")
+
+#write and then check data quality, pick 1 station for each site.
+#write.csv(closest_stations, "C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\climate\\close_stations.csv")
+
+touse<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\climate\\close_stations.csv")%>%
+  filter(use!=0)
+
+token <-'qyXXjlaezYAyeQyiMoKPbIYEQYgCfrch'
+options(noaakey=token)
+
+ppt.all<-data.frame()
+stations_touse<-touse$station
+
+for (i in 1:length(stations_touse)){
+  station<-stations_touse[i]
+  ppt.station<-ghcnd_search(station, var = "PRCP")
+  ppt.station<-as.data.frame(ppt.station)
+  ppt.all<-rbind(ppt.all, ppt.station)
+  
+}
+
+#create columns for year, month, day and sort
+ppt.all$year<-year(ppt.all$prcp.date)
+ppt.all$month<-month(ppt.all$prcp.date)
+ppt.all$day<-day(ppt.all$prcp.date)
+
+write.csv(ppt.all, "C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\climate\\precip_data.csv")
+ppt.all<-read.csv("C:\\Users\\megha\\Dropbox\\converge_diverge\\datasets\\LongForm\\climate\\precip_data.csv")
+
+##drop failed quality control data
+ppt.all2<-ppt.all%>%
+  filter(prcp.qflag==" ")%>%
+  na.omit()
+
+
+#drop years with less than 90% of data
+check<-ppt.all2%>%
+  group_by(prcp.id, year)%>%
+  summarize(num=length(prcp.prcp))%>%
+  filter(num>330)%>%# each year needs >90% of data
+  filter(year>1980)%>%
+  summarise(num2=length(num))
+
+check1<-ppt.all2%>%
+  group_by(prcp.id, year)%>%
+  summarize(num=length(prcp.prcp))%>%
+  filter(num>330)
+
+
+###i went through and flagged sites for which there is okay data (btwn 1-3 years of missing data or probably when less than 90% of the data were not there, and bad data, most of the data is missing and good data. This is called close_stations_R2.
+
+##for not I am giving up on this for the ANPP paper and using prizm for the US sites and emailing around for the 3 non-us locations.
